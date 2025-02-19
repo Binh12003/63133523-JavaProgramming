db58f40c348a6781eac6e8c4ed6d45d29ad037d5
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-02-19 20:03:29 +0700

    Cập nhật code
    Sửa đổi và thêm

--- HelloWorld/src/till/edu/XinChao.java
+++ HelloWorld/src/till/edu/XinChao.java
@@ -13,7 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quản lý mã nguồn GIT");
+		System.out.println("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn GIT");
+		System.out.println("Tôi thêm một dòng mới và chỉnh sửa chữ Java ở trên");
 	}
 
 }
