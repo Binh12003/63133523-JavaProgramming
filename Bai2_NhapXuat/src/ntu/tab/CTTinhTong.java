ef5e78f692ac15acd6e4f76291479bd5463bd7bc
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-02-19 20:58:26 +0700

    nhập xuất
    thêm phép tính

--- Bai2_NhapXuat/src/ntu/tab/CTTinhTong.java
+++ Bai2_NhapXuat/src/ntu/tab/CTTinhTong.java
@@ -1,9 +1,26 @@
 package ntu.tab;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// In một dòng tiêu đề App
+		System.out.print("CT TINH TONG \n");
+		
+		// chuẩn bị việc nhập
+		Scanner banPhim = new Scanner(System.in); //scan từ bàn phím
+		
+		// In dòng hướng dẫn mời nhập A
+		System.out.println("Mời nhập a: ");
+		double a = banPhim.nextDouble(); // khai báo biến double (số nguyên)
+		System.out.println("Mời nhập b: ");
+		double b = banPhim.nextDouble();
+		// Tính toán
+		double tong = a+ b;
+		// xuất kết quả
+		System.out.print("Tổng là: ");
+		System.out.print(tong);
 
 	}
 
