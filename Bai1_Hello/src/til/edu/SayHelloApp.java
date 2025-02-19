9f8c31a6ff575fd710a2a8ab686904f79b34fcc0
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-02-19 20:11:34 +0700

    Bài 1
    code bài 1: giới thiệu

---
+++ Bai1_Hello/src/til/edu/SayHelloApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package til.edu;
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la sv khoa 63CNTT: Hoc lap trinh Java");
+		System.out.print("Trương Anh Bình");
+
+	}
+
+}
