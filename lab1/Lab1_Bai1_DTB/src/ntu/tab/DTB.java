866eb055ce013c61fe171ac7eb5d945ff17f497b
63133523-JavaProggramming <btruonganh2003@gmail.com>
2025-02-20 08:07:36 +0700

    lab1 bai 1
    nhap ten va diem trung binh

---
+++ lab1/Lab1_Bai1_DTB/src/ntu/tab/DTB.java
@@ -0,0 +1,24 @@
+package ntu.tab;
+import java.util.Scanner;
+public class DTB {
+
+	public static void main(String[] args) {
+		// Scan tu ban phim
+		Scanner banPhim = new Scanner(System.in);
+		
+		// Nhap ho và ten sinh vien
+        System.out.print("Nhập họ và tên sinh viên: ");
+        String fullName = banPhim.nextLine();
+        
+        // Nhap diem trung binh
+        System.out.print("Nhập điểm trung bình: ");
+        double diemTP = banPhim.nextDouble();
+        
+        // Xuat
+        System.out.println("\nThông tin sinh viên:");
+        System.out.println("Họ và tên: " + fullName);
+        System.out.println("Điểm trung bình: " + diemTP);
+
+	}
+
+}
